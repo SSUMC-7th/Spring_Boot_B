3253440f5dba2106a5c92e54687925498b6ed4ff
Jangwon0319 <ryujw0319@gmail.com>
2024-10-31 00:59:11 +0900

    ✨ Practice(#5) : 단방향 매핑 설정

--- src/main/java/umc/spring/domain/mapping/MemberAgree.java
+++ src/main/java/umc/spring/domain/mapping/MemberAgree.java
@@ -1,10 +1,9 @@
 package umc.spring.domain.mapping;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import umc.spring.domain.Member;
+import umc.spring.domain.Terms;
 import umc.spring.domain.common.BaseEntity;
 
 @Entity
@@ -17,6 +16,14 @@ public class MemberAgree extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
 
 
 }
