387c007cf04a7439071376dd5249d8f23fd0d427
Jangwon0319 <ryujw0319@gmail.com>
2024-10-31 01:06:39 +0900

    ✨ Practice : 양방향 매핑 설정

--- src/main/java/umc/spring/domain/mapping/MemberMission.java
+++ src/main/java/umc/spring/domain/mapping/MemberMission.java
@@ -2,6 +2,9 @@ package umc.spring.domain.mapping;
 
 import jakarta.persistence.*;
 import lombok.*;
+import umc.spring.domain.Member;
+import umc.spring.domain.Mission;
+import umc.spring.domain.Store;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.enums.MissionStatus;
 
@@ -18,4 +21,12 @@ public class MemberMission extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private MissionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
 }
