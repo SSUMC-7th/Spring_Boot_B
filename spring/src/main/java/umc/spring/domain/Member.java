2c7e62a3ab85306d141ae30722ac5861b638ebee
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-10-31 11:35:50 +0900

    :sparkles: Practice(#5) : Member 추가

---
+++ spring/src/main/java/umc/spring/domain/Member.java
@@ -0,0 +1,58 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.Gender;
+import umc.spring.domain.enums.MemberStatus;
+import umc.spring.domain.enums.SocialType;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String userID;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    private String name;
+
+    private String address;
+
+    private String birth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+
+    private String tel;
+
+    /*
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+     */
+}
