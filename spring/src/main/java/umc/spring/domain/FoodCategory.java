6aa3334166fa16e3e624f792f767ed185e004574
2ghrms <aa020228@gmail.com>
2024-10-31 20:09:39 +0900

    :sparkles:Practice(#5) : design mapping tables & edit small details

--- spring/src/main/java/umc/spring/domain/FoodCategory.java
+++ spring/src/main/java/umc/spring/domain/FoodCategory.java
@@ -3,21 +3,20 @@ package umc.spring.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import umc.spring.domain.common.BaseEntity;
-import umc.spring.domain.enums.*;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Food extends BaseEntity {
+public class FoodCategory extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private FoodCategory foodCategory;
+    private umc.spring.domain.enums.FoodCategory foodCategory;
 
     private Boolean preferOptional;
 }
