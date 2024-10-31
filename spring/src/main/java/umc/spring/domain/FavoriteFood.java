1c61604e8f7ecbbeafe89847f8b2c888f7d0da15
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-10-31 12:03:48 +0900

    :sparkles: Practice(#5) : 선호음식 테이블

---
+++ spring/src/main/java/umc/spring/domain/FavoriteFood.java
@@ -0,0 +1,27 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.Food;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FavoriteFood extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Food food;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+}
---
+++ spring/src/main/java/umc/spring/domain/enums/Food.java
@@ -0,0 +1,6 @@
+package umc.spring.domain.enums;
+
+public enum Food {
+    KF, JF, CF, WF, CHICKEN, STREET, MEAT, LUNCHBOX,
+    NIGHTFOOD, FASTFOOD, DESSERT, AF
+}
