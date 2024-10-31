b8acbc302d8085cc3a5328cb9a412e38954e1541
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-10-31 12:39:11 +0900

    :sparkles: Practice(#5) : Mission 테이블

---
+++ spring/src/main/java/umc/spring/domain/Mission.java
@@ -0,0 +1,26 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer missionPoint;
+
+    private LocalDate deadline;
+
+    private String content;
+}
