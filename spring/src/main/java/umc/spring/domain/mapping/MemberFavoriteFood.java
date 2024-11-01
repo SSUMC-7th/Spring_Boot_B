7a74943c44f72869065c99f82ef133fe17c27cfb
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-01 17:51:43 +0900

    :sparkles: Mission(#5) : 매핑테이블 추가

---
+++ spring/src/main/java/umc/spring/domain/mapping/MemberFavoriteFood.java
@@ -0,0 +1,27 @@
+package umc.spring.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.FavoriteFood;
+import umc.spring.domain.Member;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberFavoriteFood extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "favoriteFood_id")
+    private FavoriteFood favoriteFood;
+}
