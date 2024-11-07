980c5236d7cacc37187eda307f89fe0a2f3875ba
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-07 23:31:28 +0900

    :sparkles: Practice(#6) : RestaurantService 구현

---
+++ spring/src/main/java/umc/spring/service/restaurantService/RestaurantQueryService.java
@@ -0,0 +1,12 @@
+package umc.spring.service.restaurantService;
+
+import umc.spring.domain.Restaurant;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantQueryService {
+
+    Optional<Restaurant> findRestaurant(Long id);
+    List<Restaurant> findRestaurantsByNameAndScore(String name, Float score);
+}
