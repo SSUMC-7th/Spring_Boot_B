d62f8727558fbdc1c1f002302349af79ea7576d6
2ghrms <aa020228@gmail.com>
2024-11-14 14:29:43 +0900

    :sparkles:practice(#7) : Write ExceptionAdvice

--- spring/src/main/java/umc/spring/apiPayload/exception/GeneralException.java
+++ spring/src/main/java/umc/spring/apiPayload/exception/GeneralException.java
@@ -7,7 +7,7 @@ import umc.spring.apiPayload.code.ErrorReasonDTO;
 
 @Getter
 @AllArgsConstructor
-public class GeneralException {
+public class GeneralException extends RuntimeException{
 
     private BaseErrorCode code;
 
