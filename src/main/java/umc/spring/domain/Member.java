8161beb0988abc8975c504f4ab3d1a812fa255d5
Jangwon0319 <ryujw0319@gmail.com>
2024-11-19 20:31:12 +0900

    ✨ Practice(#8) : 회원가입(간략화) API 구현

--- src/main/java/umc/spring/domain/Member.java
+++ src/main/java/umc/spring/domain/Member.java
@@ -46,7 +46,7 @@ public class Member extends BaseEntity {
 
     private LocalDate inactiveDate;
 
-    @Column(nullable = false, length = 50)
+    // @Column(nullable = false, length = 50)
     private String email;
 
     private Integer point;
