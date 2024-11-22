1a036bf4391aefad57ddbb580899e00d9b13c916
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-22 16:23:48 +0900

    :sparkles: Feat: annotation 추가

---
+++ spring/src/main/java/umc/spring/handler/resolver/RestaurantExistsValidator.java
@@ -0,0 +1,24 @@
+package umc.spring.handler.resolver;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import umc.spring.handler.annotation.RestaurantExists;
+import umc.spring.repository.restaurantRepository.RestaurantRepository;
+
+public class RestaurantExistsValidator implements ConstraintValidator<RestaurantExists, Long> {
+
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantExistsValidator(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public boolean isValid(Long restaurantId, ConstraintValidatorContext context) {
+        if (restaurantId == null) {
+            return false;
+        }
+        return restaurantRepository.existsById(restaurantId);
+    }
+}
+
