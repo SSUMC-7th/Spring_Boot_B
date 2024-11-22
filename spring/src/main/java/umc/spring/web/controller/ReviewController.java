3356122c1c0e1fe50894203026ec73a45a7f564b
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-22 16:33:17 +0900

    :sparkles: Feat: Valid 어노테이션 추가

--- spring/src/main/java/umc/spring/web/controller/ReviewController.java
+++ spring/src/main/java/umc/spring/web/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package umc.spring.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,7 @@ public class ReviewController {
 
     @Operation(summary = "리뷰 등록", description = "레스토랑 아이디로 리뷰 등록 api입니다.")
     @PostMapping("/add")
-    public ApiResponse<ReviewResponseDTO.ReviewIdDTO> addReview(@RequestBody ReviewRequestDTO.AddReviewDTO request) {
+    public ApiResponse<ReviewResponseDTO.ReviewIdDTO> addReview(@Valid @RequestBody ReviewRequestDTO.AddReviewDTO request) {
         Review review = reviewService.addReview(request);
         ReviewResponseDTO.ReviewIdDTO response = ReviewConverter.toReviewIdDTO(review);
         return ApiResponse.of(SuccessStatus.REVIEW_CREATE_OK, response);
