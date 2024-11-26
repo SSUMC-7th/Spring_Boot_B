3d7c55faf2f1ac0a56efceee24d2c7e70ba3879f
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-26 18:16:23 +0900

    :sparkles: Feat: MissionController 구현

--- spring/src/main/java/umc/spring/apiPayload/code/status/SuccessStatus.java
+++ spring/src/main/java/umc/spring/apiPayload/code/status/SuccessStatus.java
@@ -12,8 +12,10 @@ public enum SuccessStatus implements BaseCode {
     _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
     // 멤버 관련 응답
     // 리뷰 관련 응답
-    REVIEW_CREATE_OK(HttpStatus.OK, "REVIEW201", "리뷰 등록 성공");
+    REVIEW_CREATE_OK(HttpStatus.OK, "REVIEW201", "리뷰 등록 성공"),
 
+    // 미션 관련 응답
+    MISSION_CREATE_OK(HttpStatus.OK, "MISSION201", "미션 등록 성공");
 
     private final HttpStatus httpStatus;
     private final String code;
