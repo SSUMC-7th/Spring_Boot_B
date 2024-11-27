428d0696550e05666406391d4df4418cdda083b0
Jangwon0319 <ryujw0319@gmail.com>
2024-11-27 18:07:00 +0900

    ✨ Practice(#9) : 리뷰 목록 조회 API 구현

---
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -0,0 +1,35 @@
+package umc.spring.converter;
+
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
+import umc.spring.web.dto.StoreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StoreConverter {
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .body(review.getBody())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
+                .build();
+    }
+}
--- src/main/java/umc/spring/service/StoreService/StoreQueryService.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryService.java
@@ -1,5 +1,8 @@
 package umc.spring.service.StoreService;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 
 import java.util.List;
@@ -9,4 +12,6 @@ public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
     List<Store> findStoresByNameAndScore(String name, Float score);
+
+    Page<Review> getReviewList(Long storeId, Integer page);
 }
