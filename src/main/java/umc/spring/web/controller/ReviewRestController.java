2c1988185af1435bd6f06aa4975b23de2b07ce51
Jangwon0319 <ryujw0319@gmail.com>
2024-11-27 20:00:31 +0900

    ✨ Feat : 내가 작성한 리뷰 목록 API 구현

--- src/main/java/umc/spring/web/controller/ReviewRestController.java
+++ src/main/java/umc/spring/web/controller/ReviewRestController.java
@@ -1,17 +1,21 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.ReviewConverter;
+import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Review;
 import umc.spring.service.ReviewService.ReviewCommandService;
+import umc.spring.service.ReviewService.ReviewQueryService;
+import umc.spring.service.StoreService.StoreQueryService;
+import umc.spring.validation.annotation.CheckPage;
 import umc.spring.web.dto.ReviewRequestDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
+import umc.spring.web.dto.StoreResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +24,20 @@ public class ReviewRestController {
 
     private final ReviewCommandService reviewCommandService;
 
+    private final ReviewQueryService reviewQueryService;
+
     @PostMapping("/create")
     public ApiResponse<ReviewResponseDTO.CreateReviewResultDTO> createReview(@RequestBody ReviewRequestDTO.CreateReviewDto request) {
         Review review = reviewCommandService.createReview(request);
         return ApiResponse.onSuccess(ReviewConverter.toCreateReviewResultDto(review));
+    }
+
+    @GetMapping("/my-reviews")
+    @Operation(summary = "내가 작성한 리뷰 목록 조회", description = "사용자가 작성한 리뷰 목록을 조회합니다. 페이지는 1부터 시작합니다.")
+    public ApiResponse<StoreResponseDTO.ReviewPreViewListDTO> getMyReviewList(
+            @RequestParam(name = "memberId") Long memberId,
+            @RequestParam(name = "page") @CheckPage Integer page) {
+        Page<Review> reviewList = reviewQueryService.getMyReviews(memberId,page - 1);
+        return ApiResponse.onSuccess(StoreConverter.reviewPreViewListDTO(reviewList));
     }
 }
