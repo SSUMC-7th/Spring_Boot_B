d2eb5169b5f70f820281b7030c9b4b4b039d80f2
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-27 16:35:23 +0900

    :sparkles: Feat: ValidatorConfig 설정

--- spring/src/main/java/umc/spring/handler/resolver/RestaurantExistsValidator.java
+++ spring/src/main/java/umc/spring/handler/resolver/RestaurantExistsValidator.java
@@ -2,9 +2,11 @@ package umc.spring.handler.resolver;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
 import umc.spring.handler.annotation.RestaurantExists;
 import umc.spring.repository.restaurantRepository.RestaurantRepository;
 
+@Component
 public class RestaurantExistsValidator implements ConstraintValidator<RestaurantExists, Long> {
 
     private final RestaurantRepository restaurantRepository;
