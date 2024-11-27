79a161a73d6974f4fd9ee6530b17049492573724
Jangwon0319 <ryujw0319@gmail.com>
2024-11-27 20:25:39 +0900

    ✨ Feat : 특정 가게 미션 목록 API 구현

--- src/main/java/umc/spring/service/MissionService/MissionQueryService.java
+++ src/main/java/umc/spring/service/MissionService/MissionQueryService.java
@@ -1,5 +1,6 @@
 package umc.spring.service.MissionService;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
 import umc.spring.domain.Store;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface MissionQueryService {
     Optional<Mission> findMission(Long id);
     List<Mission> findInProgressMissionsByMemberWithCursor(Long memberId, Long cursorId, int limit);
+
+    Page<Mission> getMissionsByStore(Long storeId, Integer page);
 }
