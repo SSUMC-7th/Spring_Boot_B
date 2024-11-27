2a1fdbb7be140c9015d9fd7c33afee6c46a1533c
2ghrms <aa020228@gmail.com>
2024-11-28 02:59:39 +0900

    :sparkles:feat(#77) : 특정 가게의 미션 목록 API 구현

--- spring/src/main/java/umc/spring/web/controller/MissionRestController.java
+++ spring/src/main/java/umc/spring/web/controller/MissionRestController.java
@@ -1,29 +1,50 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MissionConverter;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.service.MissionService.MissionCommandService;
+import umc.spring.service.MissionService.MissionQueryService;
+import umc.spring.validation.annotation.CheckPage;
+import umc.spring.validation.annotation.ExistStore;
 import umc.spring.web.dto.MissionRequestDTO;
 import umc.spring.web.dto.MissionResponseDTO;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/missions")
 public class MissionRestController {
 
     private final MissionCommandService missionCommandService;
+    private final MissionQueryService missionQueryService;
 
     @PostMapping("/")
     public ApiResponse<MissionResponseDTO.CreateMissionResultDto> addMission(@RequestBody @Valid MissionRequestDTO.CreateMissionDto request) {
         Mission mission = missionCommandService.addMission(request);
         return ApiResponse.onSuccess(MissionConverter.toCreateResultDto(mission));
+    }
+
+    @GetMapping("/{storeId}/missions")
+    @Operation(summary = "특정 가게의 미션 목록 조회", description = "특정 가게의 미션 목록을 페이징하여 조회합니다.")
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "페이지 번호, query string 입니다!")
+    })
+    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getMissionsByStore(@PathVariable Long storeId, @CheckPage @RequestParam Integer page) {
+        int actualPage = page - 1;
+        Page<Mission> missions = missionQueryService.getMissionsByStore(storeId, actualPage);
+        return ApiResponse.onSuccess(MissionConverter.missionPreViewListDTO(missions));
     }
 }
 
