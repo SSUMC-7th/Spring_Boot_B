b674ce4575c3711e341cbe38417ab66e091d4a63
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-28 20:54:33 +0900

    :sparkles: Feat: Service 구현

--- spring/src/main/java/umc/spring/service/memberMissionService/MemberMissionService.java
+++ spring/src/main/java/umc/spring/service/memberMissionService/MemberMissionService.java
@@ -1,9 +1,12 @@
 package umc.spring.service.memberMissionService;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.dto.memberMissionDTO.MemberMissionRequestDTO;
 
 public interface MemberMissionService {
     MemberMission addMemberMission(MemberMissionRequestDTO.AddMemberMissionDTO request);
+    Page<MemberMission> getMissionListByMemberId(Long memberId, Integer page, MissionStatus status);
 }
--- spring/src/main/java/umc/spring/service/memberMissionService/MemberMissionServiceImpl.java
+++ spring/src/main/java/umc/spring/service/memberMissionService/MemberMissionServiceImpl.java
@@ -1,10 +1,13 @@
 package umc.spring.service.memberMissionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.spring.converter.MemberMissionConverter;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Restaurant;
 import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.dto.memberMissionDTO.MemberMissionRequestDTO;
@@ -29,5 +32,13 @@ public class MemberMissionServiceImpl implements MemberMissionService{
         MemberMission newMemberMission = MemberMissionConverter.toMemberMission(member, mission, MissionStatus.CHALLENGING);
         memberMissionRepository.save(newMemberMission);
         return newMemberMission;
+    }
+
+    @Override
+    public Page<MemberMission> getMissionListByMemberId(Long memberId, Integer page, MissionStatus status){
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow();
+
+        return memberMissionRepository.findAllByMemberAndStatus(member, status, PageRequest.of(page, 10));
     }
 }
