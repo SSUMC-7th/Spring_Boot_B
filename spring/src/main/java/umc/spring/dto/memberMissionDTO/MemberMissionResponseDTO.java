c6c1a2b3652538281b1786ca56210c5bee5b3272
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-28 21:09:35 +0900

    Revert ":sparkles: Feat: DTO 생성"
    This reverts commit aef45a13ee57c8fc6d786b8bf553d9e3330926e0.

--- spring/src/main/java/umc/spring/dto/memberMissionDTO/MemberMissionResponseDTO.java
+++ spring/src/main/java/umc/spring/dto/memberMissionDTO/MemberMissionResponseDTO.java
@@ -1,10 +1,6 @@
 package umc.spring.dto.memberMissionDTO;
 
 import lombok.*;
-import umc.spring.dto.missionDTO.MissionResponseDTO;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Data
 public class MemberMissionResponseDTO {
@@ -14,29 +10,5 @@ public class MemberMissionResponseDTO {
     @AllArgsConstructor
     public static class MemberMissionIdDTO {
         private Long memberMissionId;
-    }
-
-    @Builder
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class MemberMissionListDTO {
-        List<MemberMissionResponseDTO.MemberMissionPreViewDTO> missionList;
-        Integer listSize;
-        Integer totalPage;
-        Long totalElements;
-        Boolean isFirst;
-        Boolean isLast;
-    }
-
-    @Builder
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class MemberMissionPreViewDTO {
-        Integer missionPoint;
-        LocalDate deadline;
-        String content;
-        String restaurantName;
     }
 }
