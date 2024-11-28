4c25564008dab416297048f102938721514ee5a0
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-28 20:08:36 +0900

    :sparkles: Feat: DTO 생성

--- spring/src/main/java/umc/spring/dto/missionDTO/MissionResponseDTO.java
+++ spring/src/main/java/umc/spring/dto/missionDTO/MissionResponseDTO.java
@@ -2,6 +2,9 @@ package umc.spring.dto.missionDTO;
 
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 public class MissionResponseDTO {
     @Getter
@@ -10,5 +13,28 @@ public class MissionResponseDTO {
     @AllArgsConstructor
     public static class MissionIdDTO {
         Long missionId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RestaurantMissionListDTO {
+        List<MissionResponseDTO.RestaurantMissionPreViewDTO> missionList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RestaurantMissionPreViewDTO {
+        Integer missionPoint;
+        LocalDate deadline;
+        String content;
     }
 }
