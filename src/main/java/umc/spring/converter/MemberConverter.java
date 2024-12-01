b31231afe31103d911267e74b8297b3e78b409d5
Jangwon0319 <ryujw0319@gmail.com>
2024-12-01 12:57:14 +0900

    ✨ Feat(#10) : 회원가입 보안 추가

--- src/main/java/umc/spring/converter/MemberConverter.java
+++ src/main/java/umc/spring/converter/MemberConverter.java
@@ -33,10 +33,13 @@ public class MemberConverter {
         }
 
         return Member.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
                 .address(request.getAddress())
                 .specAddress(request.getSpecAddress())
                 .gender(gender)
                 .name(request.getName())
+                .role(request.getRole())
                 .memberPreferList(new ArrayList<>())
                 .build();
     }
