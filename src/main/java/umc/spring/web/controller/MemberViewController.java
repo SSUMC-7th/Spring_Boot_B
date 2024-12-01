231641ea11e17dd8eddc572e79dd1ea9704b1760
Jangwon0319 <ryujw0319@gmail.com>
2024-12-01 14:38:14 +0900

    ✨ Feat(#10) : 회원가입 테스트 구현

--- src/main/java/umc/spring/web/controller/MemberViewController.java
+++ src/main/java/umc/spring/web/controller/MemberViewController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import umc.spring.service.MemberService.MemberCommandService;
 import umc.spring.web.dto.MemberRequestDTO;
 
@@ -22,7 +21,7 @@ public class MemberViewController {
                              BindingResult bindingResult,
                              Model model) {
         if (bindingResult.hasErrors()) {
-            // 뷰에 데이터 바인딩이 실패할 경우 signup 페이지를 유지
+            // 뷰에 데이터 바인딩이 실패할 경우 signup 페이지를 유지합니다.
             return "signup";
         }
 
@@ -30,7 +29,7 @@ public class MemberViewController {
             memberCommandService.joinMember(request);
             return "redirect:/login";
         } catch (Exception e) {
-            // 회원가입 과정에서 에러가 발생할 경우 에러 메세지를 보내고, signup 페이지 유지
+            // 회원가입 과정에서 에러가 발생할 경우 에러 메시지를 보내고, signup 페이디를 유지합니다.
             model.addAttribute("error", e.getMessage());
             return "signup";
         }
